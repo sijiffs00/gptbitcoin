49f85a5e29034dfec6d5bfd8817aa7cf516a05bd
sojung0219 <sj.lee@megaworks.ai>
2024-12-13 23:18:36 +0900

    0

---
+++ mvp.py
@@ -0,0 +1,74 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def ai_trading():
+    # 1. 업비트 차트 데이터 가져오기 (30일 일봉)
+    import pyupbit
+    df = pyupbit.get_ohlcv("KRW-BTC", count=30, interval="day")
+
+    # 2. AI에게 데이터 제공하고 판단 받기
+    from openai import OpenAI
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+        "role": "system",
+        "content": [
+            {
+            "type": "text",
+            "text": "You are an expert in Bitcoin investing. Tell me whether to buy, sell, or hold at the moment based on the chart data provided. response in json format.\n\nResponse Example:\n{\"decision\": \"buy\", \"reason\": \"some technical reason\"}\n{\"decision\": \"sell\", \"reason\": \"some technical reason\"}\n{\"decision\": \"hold\", \"reason\": \"some technical reason\"}"
+            }
+        ]
+        },
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": df.to_json()
+            }
+        ]
+        }
+    ],
+    response_format={
+        "type": "json_object"
+    }
+    )
+    result = response.choices[0].message.content
+
+    # 3. AI의 판단에 따라 실제로 자동매매 진행하기
+    import json
+    result = json.loads(result)
+    import pyupbit
+    access = os.getenv("UPBIT_ACCESS_KEY")
+    secret = os.getenv("UPBIT_SECRET_KEY")
+    upbit = pyupbit.Upbit(access, secret)
+
+    print("### AI Decision: ", result["decision"].upper(), "###")
+    print(f"### Reason: {result['reason']} ###")
+
+    if result["decision"] == "buy":
+        my_krw = upbit.get_balance("KRW")
+        if my_krw*0.9995 > 5000:
+            print("### Buy Order Executed ###")
+            print(upbit.buy_market_order("KRW-BTC", my_krw * 0.9995))
+        else:
+            print("### Buy Order Failed: Insufficient KRW (less than 5000 KRW) ###")
+    elif result["decision"] == "sell":
+        my_btc = upbit.get_balance("KRW-BTC")
+        current_price = pyupbit.get_orderbook(ticker="KRW-BTC")['orderbook_units'][0]["ask_price"]
+        if my_btc*current_price > 5000:
+            print("### Sell Order Executed ###")
+            print(upbit.sell_market_order("KRW-BTC", my_btc))
+        else:
+            print("### Sell Order Failed: Insufficient BTC (less than 5000 KRW worth) ###")
+    elif result["decision"] == "hold":
+        print("### Hold Position ###")
+        
+while True:
+    import time
+    time.sleep(10)
+    ai_trading()
---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv("UPBIT_ACCESS_KEY"))
+print(os.getenv("UPBIT_SECRET_KRY"))
+print(os.getenv("OPENAI_API_KEY"))
+
+
+# 너는 비트코인 투자 전문가야.
+# 제공된 차트 데이터를 기반으로 현재 buy, sell, hold 중 어떤 선택을 해야할지 알려줘
+# JSON 형식으로 대답해
+
+# You are an expert in Bitcoin investing. Tell me whether to buy, sell, or hold at the moment based on the chart data provided.
+# JSON 형식으로 대답해
+# Response Example : {"decision": "buy", "reason": "some technical reason"}
+
+
