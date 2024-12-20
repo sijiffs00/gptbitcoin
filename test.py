265bd0503121bde3693d2bbb14b7bb69d31d6e28
sojung0219 <sj.lee@megaworks.ai>
2024-12-20 17:00:57 +0900

    .env파일에 정리한 환경변수를 print로 출력해본다

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print(os.getenv("UPBIT_ACCESS_KEY"))
-print(os.getenv("UPBIT_SECRET_KRY"))
+print(os.getenv("UPBIT_SECRET_KEY"))
 print(os.getenv("OPENAI_API_KEY"))
 
 
