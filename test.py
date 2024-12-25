118d873e621574fe5c86b0e6c52fb7bbc037cdda
sojung0219 <sj.lee@megaworks.ai>
2024-12-25 18:15:01 +0900

    업비트차트데이터를 가져와서 gpt API요청할때 넣어서 보낸다.

--- test.py
+++ test.py
@@ -8,33 +8,49 @@ load_dotenv()
 import pyupbit
 df = pyupbit.get_ohlcv("KRW-BTC", count=30, interval="day")
 # print(df.to_json())
-print(os.getenv('UPBIT_SECRET_KEY'))  
-print(os.getenv('OPENAI_API_KEY'))  
+
 
 # 2. AI에게 데이터 제공하고 판단 받기
+from openai import OpenAI
+client = OpenAI()
 
-# from openai import OpenAI
-# client = OpenAI()
-
-# response = client.chat.completions.create(
-#   model="gpt-4",
-#   messages=[
-#     {
-#       "role": "system",
-#       "content": [
-#         {
-#           "type": "text",
-#           "text": "You are an expert in Bitcoin investing.\nTell me whether to buy, sell, or hold at the moment based on the chart data provided.\nresponse in json format.\n\n    Response Example :\n    {\"decision\": \"buy\", \"reason\": \"some technical reason\"},\n    {\"decision\": \"buy\", \"reason\": \"some technical reason\"},\n    {\"decision\": \"buy\", \"reason\": \"some technical reason\"}"
-#         }
-#       ]
-#     }
-#   ],
-#   response_format={
-#     "type": "json_object"
-#   },
-#   temperature=1,
-#   max_completion_tokens=2048,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0
-# )
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {
+      "role": "system",
+      "content": [
+        {
+          "type": "text",
+          "text": "You are an expert in Bitcoin investing.\nTell me whether to buy, sell, or hold at the moment based on the chart data provided.\nresponse in json format.\n\nResponse Example :\n{\"decision\": \"buy\", \"reason\": \"some technical reason\"},\n{\"decision\": \"buy\", \"reason\": \"some technical reason\"},\n{\"decision\": \"buy\", \"reason\": \"some technical reason\"},"
+        }
+      ]
+    },
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": df.to_json()
+        }
+      ]
+    },
+    {
+      "role": "assistant",
+      "content": [
+        {
+          "type": "text",
+          "text": "{\"decision\": \"hold\", \"reason\": \"Bitcoin prices have shown some volatility but appear to be stabilizing. After a peak, there was a slight decline, and the volume of trading is decreasing. This might indicate consolidation before another move. With no major sell-off or breakout signals, holding is advisable.\"}"
+        }
+      ]
+    }
+  ],
+  response_format={
+    "type": "json_object"
+  },
+  temperature=1,
+  max_completion_tokens=2048,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
