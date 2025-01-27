d291f0bdfef75569a91fc04e85730e916d99f3fd
sijiffs00 <sijiffs00@gmail.com>
2025-01-27 15:17:10 +0900

    워뇨띠매매법 유튜브영상의 한글자막을 가져와 영상자막 하나로 쭉 뽑기

--- ytb_script_test.py
+++ ytb_script_test.py
@@ -1,8 +1,12 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled
 
+
+# 지금 한글자막을 가져오고있는 영상은 '워뇨띠 매매법' 영상임.
+
 try:
-    transcript = YouTubeTranscriptApi.get_transcript("-59bKxwir5Q")
+    # 'ko'를 넣어서 한국어 자막을 가져오도록 설정했어!
+    transcript = YouTubeTranscriptApi.get_transcript("EiDXQmOQ6_o", languages=['ko'])
     # text만 뽑아서 하나의 문자열로 만들기
     full_text = ' '.join([line['text'] for line in transcript])
     print(full_text)
